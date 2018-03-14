9f48c1ca15bbb8cc92cbd1d1972dd32ff0e1d30d
domlockett <34702780+domlockett@users.noreply.github.com>
2018-03-14 17:26:52 -0500

    Issues
    So I had a bunch of issues after creating the skeleton and I had to rearrange the directory and I am not really sure how to fix it so I'm just dropping the new files into the online git in hopes that all the issues I caused go away...

---
+++ MIdterm/Development.R
@@ -0,0 +1,13 @@
+setwd("C:/Users/Dominique/OneDrive/Documents/Spring 2018/Applied Statistical Programming/git/Week 9/Midterm")
+# load the libraries that'll do all the work
+library(devtools)
+library(roxygen2)
+rm(list=ls())
+
+#now we're gonna run skeleton to set up the bones of our package
+
+package.skeleton()
+current.code<-as.package("anRpackage")
+load_all(current.code)
+document(current.code)
+check(current.code)
